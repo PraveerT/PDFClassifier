319f4b89c1a90dd05db058fb6558943b4b65c999
PraveerT <praveertowakel@yahoo.com>
2022-04-29 01:20:55 +0400

    corrected

--- WEBPTOJPG.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-from PIL import Image
-path="C:\\Users\\prav\\Documents\\ST FINAL\\S5\\photo_2\\"
-for filename in glob.glob(os.path.join(path,'*.webp')):
-    print (filename)
-    im=Image.open(filename).convert("RGB")
-    im.save(filename+".jpg","jpeg")
-
